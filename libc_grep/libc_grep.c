2a650af10e3c71467c50a7c3e7a453907bb4d921
Aleksey Cheusov <vle@gmx.net>
2024-10-05 19:27:33 +0400

    regexec-based tools: add option -o for searching substring using regexp

--- libc_grep/libc_grep.c
+++ libc_grep/libc_grep.c
@@ -24,44 +24,100 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <regex.h>
+#include <getopt.h>
 
 #include "file_match.h"
 
+#ifndef REG_ONESUB /* defined by UXRE */
+#define REG_ONESUB 0
+#endif
+
 static regex_t regex;
 
-static void match(const char *line)
+static void reg_match(const char *line)
 {
-	int reti = regexec(&regex, line, 0, NULL, 0);
-	if (reti == 0) {
+	int ret = regexec(&regex, line, 0, NULL, 0);
+	if (ret == 0) {
 		// Match found, print the line
 		puts(line);
-	} else if (reti != REG_NOMATCH) {
+	} else if (ret != REG_NOMATCH) {
 		// Error occurred (other than no match)
 		fprintf(stderr, "Regex match failed\n");
 		exit(1);
 	}
 }
 
+static void reg_search(const char *line)
+{
+	regmatch_t match;
+	int ret = regexec(&regex, line, 1, &match, 0);
+	if (ret == 0) {
+		// Match found, print the substring
+		fwrite(line + match.rm_so, 1, match.rm_eo - match.rm_so, stdout);
+		putchar('\n');
+	} else if (ret != REG_NOMATCH) {
+		// Error occurred (other than no match)
+		fprintf(stderr, "Regex match failed\n");
+		exit(1);
+	}
+}
+
+static void usage(const char *progname)
+{
+	fprintf(stderr, "usage: %s [OPTIONS] REGEXP FILE\n\
+OPTIONS:\n\
+   -h            display this screen\n\
+   -o            show only the part of a matching line that matches REGEXP\n\
+\n", progname);
+}
+
 int main(int argc, char *argv[])
 {
-	int reti;
+	int ret;
+	int opt;
+	int regcomp_flags = REG_EXTENDED | REG_NOSUB;
+	int subexpr = 0;
+	const char *progname = argv[0];
 
-	if (argc < 3) {
-		fprintf(stderr, "Usage: %s <pattern> <filename>\n", argv[0]);
+	while ((opt = getopt(argc, argv, "ho")) != -1) {
+		switch (opt) {
+			case 'h':
+				usage(progname);
+				exit(0);
+				break;
+			case 'o':
+				regcomp_flags &= ~REG_NOSUB;
+				regcomp_flags |= REG_ONESUB;
+				subexpr = 1;
+				break;
+			default:
+				usage(progname);
+				exit(EXIT_FAILURE);
+		}
+	}
+
+	argc -= optind;
+	argv += optind;
+
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s <pattern> <filename>\n", progname);
 		return 1;
 	}
 
-	const char *pattern = argv[1];
-	const char *filename = argv[2];
+	const char *pattern = argv[0];
+	const char *filename = argv[1];
 
 	// Compile the regular expression
-	reti = regcomp(&regex, pattern, REG_EXTENDED | REG_NOSUB);
-	if (reti) {
+	ret = regcomp(&regex, pattern, regcomp_flags);
+	if (ret) {
 		fprintf(stderr, "Could not compile regex\n");
 		exit(1);
 	}
 
-	file_match(match, filename);
+	if (subexpr)
+		file_match(reg_search, filename);
+	else
+		file_match(reg_match, filename);
 
 	// Free regex memory
 	regfree(&regex);
